95cf12fa7ec3781d8fa45f22c98ebf87f5c5634d
VejayPersaud <35679567+VejayPersaud@users.noreply.github.com>
2024-07-08 18:32:41 -0400

    Testing and Browser are running
    Changed configuration files and now npm test and npm run dev are functional with no errors. next step is to properly connect the database.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const nextConfig = {
   webpack: (config) => {
