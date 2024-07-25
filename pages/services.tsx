07a261ed7b4dab432dffc8f27d2a61ffb5284f0f
Ozlem <95313700+OzPol@users.noreply.github.com>
2024-07-25 14:01:35 -0400

    - Fixing Heroku Deployment Error

--- pages/services.tsx
+++ pages/services.tsx
@@ -1,5 +1,5 @@
 import React,{ useEffect, useState } from 'react';
-import { useRouter } from 'next/router'; // Import useRouter for navigation
+// import { useRouter } from 'next/router'; // Import useRouter for navigation
 import { Service } from '../types/appwrite.type';
 import ServiceCard from '../components/ServiceCard';
 import { fetchAllServices } from '../components/DataServiceConsumer';
