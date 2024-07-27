3575dcec51f9bc587ad0948d1245717e61b5cdff
Ben <92957639+buppgard@users.noreply.github.com>
2024-07-27 11:48:25 -0500

    Only admins now have access to the admin dashboard page.

--- pages/adminDashboard.tsx
+++ pages/adminDashboard.tsx
@@ -1,7 +1,24 @@
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const AdminDashboard = () => {
+  const router = useRouter();
+  const [activeTab, setActiveTab] = useState('overview');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);// State to track selected provider
+
+  useEffect(() => {
+    const session = localStorage.getItem('appwriteSession');
+    const userType = localStorage.getItem('userType');
+    if (session && userType === 'Admin') {
+      setIsAuthenticated(true);
+      console.log("Authorized - User is admin.")
+    } else {
+      console.log("Not Authorized - User is not admin.")
+      setIsAuthenticated(false);
+      router.push('/');
+    }
+  }, [router]);
+  
   return (
     <div>
       <h1>Welcome to the Admin Dashboard!</h1>
@@ -9,5 +26,6 @@ const AdminDashboard = () => {
     </div>
   );
 };
+
 
 export default AdminDashboard;
