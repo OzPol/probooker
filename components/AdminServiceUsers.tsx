20e82296be4591d8825d3e0b9cadc67fe84b8007
Ben <92957639+buppgard@users.noreply.github.com>
2024-07-28 14:47:27 -0500

    All customers in database are now shown in admin dashboard.

--- components/AdminServiceUsers.tsx
+++ components/AdminServiceUsers.tsx
@@ -3,7 +3,7 @@ const AdminServiceUsers = () => {
   
   return (
     <div>
-      <h1>Here's where you'll soon be able to see all of the Service Users!</h1>
+      <h1>Heres where youll soon be able to see all of the Service Users!</h1>
       {/* Admin dashboard content */}
     </div>
   );
