6472e214760f7d40fd49a2e4d55b5fb5d0c2fc6e
Ozlem <95313700+OzPol@users.noreply.github.com>
2024-08-06 12:57:56 -0400

    -Webpack Error due to useState in Footer Fixed - Merging main in to 7.24

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,16 +12,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col min-h-screen`}>
-        <Header />
-        <main className="flex-grow">{children}</main>
-        <Footer />
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
